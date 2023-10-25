40b83c844c64872e6a9551a1baae1b57968e48e7
Anna Kim <asokim@email.unc.edu>
2023-10-24 21:28:16 -0400

    Create skeleton for case-view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { HomeComponent } from './component/home/home.component';
 import { SignupComponent } from './component/signup/signup.component';
 import { AccountComponent } from './component/account/account.component';
 import { authGuard } from './service/auth/auth.guard';
+import { CaseFormComponent } from './component/case-form/case-form.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [authGuard]},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'account', component: AccountComponent, canActivate: [authGuard] },
+  { path: 'case-view', component: CaseFormComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to home component
-  { path: '**', redirectTo: '/home'} // handle unknown paths
+  { path: '**', redirectTo: '/home'} // handle unknown paths]
 ];
 
 @NgModule({
