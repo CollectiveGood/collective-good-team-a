67d26f354ddfd5fb549be75302c46b313b997e55
adamd <adamdoyle630@gmail.com>
2023-11-01 14:40:25 -0400

    Start admin service methods

--- frontend/src/app/service/case/case.service.ts
+++ frontend/src/app/service/case/case.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Assignment, Case } from 'src/app/models';
@@ -10,24 +10,61 @@ import { environment } from 'src/environments/environment';
 export class CaseService {
   constructor(private http: HttpClient) {}
 
+  /* Admin-only - retrieve all cases in the database */
   getAllCases(): Observable<Case[]> {
     return this.http.get<Case[]>(`${environment.apiUrl}/cases`, {
       withCredentials: true,
     });
   }
 
+  /* Admin-only - retrieve all case assignments and their status */
+  getAllAssignments(): Observable<Assignment[]> {
+    return this.http.get<Assignment[]>(`${environment.apiUrl}/getAssignments`, {
+      withCredentials: true,
+    });
+  }
+
   // Sojin
+  /* Get all cases assigned to the current user */
   getAssignedCases(): Observable<Assignment[]> {
     return this.http.get<Assignment[]>(`${environment.apiUrl}/assignedCases`, {
       withCredentials: true,
     });
   }
 
+  /* Get a case by its hash value
+  *  @param hash: the hash value of the case
+  */
   getCaseAsPDF(hash: string): Observable<Blob> {
-    // Set the request options to accept PDF
     return this.http.get(`${environment.apiUrl}/getCase/${hash}`, {
       withCredentials: true,
       responseType: 'blob'
+    });
+  }
+
+  /* Admin-only - upload a case
+  *  @param file: the file to upload
+  */
+  addCase(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    return this.http.post(`${environment.apiUrl}/addCase`, formData, {
+      withCredentials: true,
+    });
+  }
+
+  /* Admin-only - assign a case to a user
+  *  @param user: the user to assign the case to
+  *  @param caseHash: the hash value of the case to assign
+  */
+  assignCase(user: string, caseHash: string): Observable<any> {
+    const formData = new FormData();
+    formData.append('user', user);
+    formData.append('case', caseHash);
+
+    return this.http.post(`${environment.apiUrl}/assignCase`, formData, {
+      withCredentials: true,
     });
   }
 }
