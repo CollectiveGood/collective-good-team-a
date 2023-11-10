e1bdb97b4e3fa936447e72ed8d08d643ad37e4b3
Chickenislife6 <tylerliuyang@gmail.com>
2023-11-09 20:18:21 -0500

    added another test

--- backend/tests/case.test.ts
+++ backend/tests/case.test.ts
@@ -9,6 +9,7 @@ process.env.DATABASE_URL =
 const prisma = new PrismaClient();
 
 const user = { username: "adam@gmail.com", password: "test" };
+const admin = { username: "admin@gmail.com", password: "admin" };
 beforeAll(async () => {
   try {
     await seedDatabase(prisma);
@@ -45,6 +46,40 @@ describe("getDetails", () => {
         .set("Cookie", cookies);
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty("email");
+    }, 10000);
+  });
+});
+
+describe("getCases", () => {
+  describe("GET /admin/case/getCases", () => {
+    it("Details", async () => {
+      const cookies = await getCookies(admin);
+      const body = {
+        isCompleted: false,
+        hasAssignments: false,
+        start: 0,
+        take: 10,
+        desc: true,
+      };
+      const response = await request(app)
+        .get("/getCases")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveLength(1);
+
+      body.hasAssignments = true;
+      const response2 = await request(app)
+        .get("/getCases")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response2.body).toHaveLength(2);
+      body.isCompleted = true;
+      const response3 = await request(app)
+        .get("/getCases")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response3.body).toHaveLength(1);
     }, 10000);
   });
 });
