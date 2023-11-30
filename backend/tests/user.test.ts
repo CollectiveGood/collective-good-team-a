b46e989b59d38e276a9bdccd6332231f380a8e20
Chickenislife6 <tylerliuyang@gmail.com>
2023-11-30 14:58:25 -0500

    added getUser test

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -18,23 +18,59 @@ beforeAll(async () => {
 });
 
 describe("Login", () => {
-    describe("POST /user/login", () => {
-      it("Login", async () => {
-        const response = await request(app).post("/login").send(user);
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("email");
-      }, 10000);
-    });
+  describe("POST /user/login", () => {
+    it("Login", async () => {
+      const response = await request(app).post("/login").send(user);
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("email");
+    }, 10000);
   });
+});
   
-  describe("getDetails", () => {
-    describe("POST /user/details", () => {
-      it("Details", async () => {
-        const response = await request(app)
-          .get("/details")
-          .set("Cookie", cookies);
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("email");
-      }, 10000);
-    });
+describe("getDetails", () => {
+  describe("POST /user/details", () => {
+    it("Details", async () => {
+      const response = await request(app)
+        .get("/details")
+        .set("Cookie", cookies);
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("email");
+    }, 10000);
   });
+});
+
+describe("getUsers", () => {
+  describe("POST /getUsers", () => {
+    it("Users", async () => {
+      const body = {
+        includeAdmins: false,
+        email: "",
+        start: 0,
+        take: 10,
+        desc: true
+      }
+      const response = await request(app)
+        .post("/getUsers")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveLength(2);
+
+      body.includeAdmins = true;
+      const response1 = await request(app)
+        .post("/getUsers")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response1.status).toBe(200);
+      expect(response1.body).toHaveLength(3);
+
+      body.email = "admin@gmail.com";
+      const response2 = await request(app)
+        .post("/getUsers")
+        .send(body)
+        .set("Cookie", cookies);
+      expect(response2.status).toBe(200);
+      expect(response2.body).toHaveLength(1);
+    }, 10000);
+  });
+});
